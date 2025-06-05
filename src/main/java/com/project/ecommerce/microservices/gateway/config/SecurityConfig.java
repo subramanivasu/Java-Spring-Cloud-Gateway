e762d43c53e61a2ff0c9b08b44f6a4136e3cad09
subramanivasu <subramaniyanvasu@gmail.com>
2025-06-05 12:30:15 +0530

    Add CORS Configuration

--- src/main/java/com/project/ecommerce/microservices/gateway/config/SecurityConfig.java
+++ src/main/java/com/project/ecommerce/microservices/gateway/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.project.ecommerce.microservices.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -21,13 +22,13 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(freeEndpoints)
-                        .permitAll()
-                        .anyRequest()
-                .authenticated())
+        return httpSecurity
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // Allow CORS preflight
+                        .requestMatchers(freeEndpoints).permitAll()
+                        .anyRequest().authenticated()
+                )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
-
     }
 }
